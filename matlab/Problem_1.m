e7926efab68d677300778419f4cc48065e321bc1
Ryan Skinner <anomaly1452@gmail.com>
2016-04-18 16:45:37 -0600

    tidy up magic numbers; reshape solution array

--- matlab/Problem_1.m
+++ matlab/Problem_1.m
@@ -27,7 +27,7 @@ pk = 14;
 
 % Polynomial chaos expansion (PCE) options for solution uhat
 p_list = 0:7;
-p_list = 0;
+p_list = 1;
 
 % Loop over total solution order (Homework's part 'b')
 for p = p_list
@@ -40,22 +40,25 @@ for p = p_list
     %%%
     % Construct the global matrix equation
     %%%
+    
+    % Sub-matrix dimension
+    Nsub = Nx-2;
 
-    K = zeros((Nx-2)*(P+1));
-    f = zeros((Nx-2)*(P+1), 1);
+    K = zeros((Nsub)*(P+1));
+    f = zeros((Nsub)*(P+1), 1);
     
     for i = 1:(P+1)
     for j = 1:i
         
         % Build the K_ij matrix
-        Kij = zeros(Nx-2);
+        Kij = zeros(Nsub);
         for k = 1:(Pk+1)
         
             % LHS sub-matrix and RHS sub-vector (K_ij and f_i)
             [dag, sub, sup, rhs] = Assemble_u(dx, u0, uf, Ki(k,:));
 
             % Build dense sub-matrix from sparse storage
-            Kk = zeros(Nx-2);
+            Kk = zeros(Nsub);
             Kk = Kk + diag(dag);
             Kk = Kk + [[zeros(1,length(sub));diag(sub)],zeros(length(sub)+1,1)];
             Kk = Kk + [zeros(length(sup)+1,1),[diag(sup);zeros(1,length(sup))]];
@@ -65,15 +68,15 @@ for p = p_list
             
             % Set f
             if i == 1
-                f(1:Nx-2,1) = rhs;
+                f(1:Nsub,1) = rhs;
             end
             
         end
         
-        A = 1 + (i-1)*(Nx-2);
-        B = 1 + (j-1)*(Nx-2);
-        Arng = A:(A+(Nx-3));
-        Brng = B:(B+(Nx-3));
+        A = 1 + (i-1)*(Nsub);
+        B = 1 + (j-1)*(Nsub);
+        Arng = A:(A+(Nsub-1));
+        Brng = B:(B+(Nsub-1));
         
         K(Arng,Brng) = Kij;
         if i ~= j
@@ -88,7 +91,8 @@ for p = p_list
     %%%
     
     u = f \ K;
-    plot(u(1:(Nx-2)))
+    u = reshape(u, Nsub, [])';
+    plot(u(1,:));
     
 end
 
